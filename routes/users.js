0fbfb2a4df1a9e6e22dc4e85824f6acac51aa1d7
SadPandaBear <lucas.lentz@hotmail.com>
2017-09-29 21:30:18 -0300

    Add screen rendering

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET login layout. */
+router.get('/login', function(req, res, next) {
+  res.render('users/login', { title: 'Login' });
+});
+
+/* GET signup layout. */
+router.get('/signup', function(req, res, next) {
+  res.render('users/signup', { title: 'Cadastro' });
 });
 
 module.exports = router;
