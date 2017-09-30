100076bd8300037df11e8f46d9d3cdc3658c2a3a
Lucas Lentz <lucas.lentz@hotmail.com>
2017-09-29 23:16:16 -0300

    camigol entao

--- routes/events.js
+++ routes/events.js
@@ -8,47 +8,37 @@ router.get('/', function(req, res, next) {
 
 /* POST register events. */
 router.get('/', function(req, res, next) {
-  console.log(req.body);
-  res.sendStatus(201);
+  res.status(200).json('Evento mockado');
 });
 
 /* GET list user events. */
 router.get('/', function(req, res, next) {
-  res.send('Evento mockado');
-  res.sendStatus(200);
+  res.status(200).json('Evento mockado');
 });
 
 /* POST recipe register. */
 router.post('/:id/recipe', function(req, res, next) {
-  console.log(req.body, req.params.id)
-  res.sendStatus(201);
+  res.status(201).json('Receita cadastrada')
 });
 
 /* GET recipes by user. */
 router.get('/:id/recipe', function(req, res, next) {
-  console.log(req.params.id)
-  res.send('Receita mockada');
-  res.sendStatus(200);
+  res.status(200).json('Receitas mockadas');
 });
 
 /* POST offers register. */
 router.post('/:id/offers', function(req, res, next) {
-  console.log(req.body, req.params.id)
-  res.sendStatus(201);
+  res.status(201).json('Oferta criada');
 });
 
 /* GET offers by ML. */
 router.get('/:id/offers', function(req, res, next) {
-  console.log(req.params.id)
-  res.send('Ofertas mockadas');
-  res.sendStatus(200);
+  res.status(200).json('Ofertas mockadas');
 });
 
 /* GET offers by ML. */
 router.get('/:id/drivers', function(req, res, next) {
-  console.log(req.params.id)
-  res.send('Motoristas disponíveis registrados');
-  res.sendStatus(200);
+  res.status(200).json('Motoristas mockados');
 });
 
 module.exports = router;
