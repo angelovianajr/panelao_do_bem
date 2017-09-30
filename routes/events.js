24c916bf0e269c562d3f18b1910cf017e9a1ee51
Lucas Lentz <lucas.lentz@hotmail.com>
2017-09-29 22:36:02 -0300

    APIs steps register event

--- routes/events.js
+++ routes/events.js
@@ -18,4 +18,37 @@ router.get('/', function(req, res, next) {
   res.sendStatus(200);
 });
 
+/* POST recipe register. */
+router.post('/:id/recipe', function(req, res, next) {
+  console.log(req.body, req.params.id)
+  res.sendStatus(201);
+});
+
+/* GET recipes by user. */
+router.get('/:id/recipe', function(req, res, next) {
+  console.log(req.params.id)
+  res.send('Receita mockada');
+  res.sendStatus(200);
+});
+
+/* POST offers register. */
+router.post('/:id/offers', function(req, res, next) {
+  console.log(req.body, req.params.id)
+  res.sendStatus(201);
+});
+
+/* GET offers by ML. */
+router.get('/:id/offers', function(req, res, next) {
+  console.log(req.params.id)
+  res.send('Ofertas mockadas');
+  res.sendStatus(200);
+});
+
+/* GET offers by ML. */
+router.get('/:id/drivers', function(req, res, next) {
+  console.log(req.params.id)
+  res.send('Motoristas disponíveis registrados');
+  res.sendStatus(200);
+});
+
 module.exports = router;
